b46c404eb0ce84c3ae4eeb76bbdc7558d0972efa
ryansaad <47457528+ryansaad@users.noreply.github.com>
2025-06-16 12:39:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const PORT = 8081;
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
@@ -14,6 +14,6 @@ app.get('/about', (req,res) => {
    
   });
 
-app.listen(8082, () => {
-    console.log('Listening on port 8082');
+app.listen(PORT, '0.0.0.0', () => { // Added '0.0.0.0' for explicit binding
+    console.log(`Node.js app listening at http://0.0.0.0:${PORT}`);
 });
